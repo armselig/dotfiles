5eccb4c0587c197e25d8d431da7d6f476dcb7cd4
Hendrik Neumann <henne@armselig.net>
2024-09-10 15:39:39 +0200

    fix(nvim): add missing <CR> to grep keymap

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -4,7 +4,7 @@ local conform = require("conform")
 -- files: find
 key.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
 -- files: grep
-key.set('n', '<leader>fg', '<cmd>Telescope live_grep')
+key.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
 -- files: browse
 key.set('n', '<leader>fb', '<cmd>NvimTreeToggle<CR>')
 -- files: prettify
