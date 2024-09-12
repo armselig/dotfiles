f2399a04d14bee7e98d7fe22f26edefc654d327b
Hendrik Neumann <henne@armselig.net>
2024-09-12 11:24:57 +0200

    feat(fish): add pnpm config

--- config.fish
+++ config.fish
@@ -83,3 +83,10 @@ function nvm
 end
 nvm use default 
 
+
+# pnpm
+set -gx PNPM_HOME "/Users/henneuma/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
