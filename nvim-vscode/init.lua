e92f45eba5a17acbf126b400bb2525e41ff5a13a
Hendrik <henne@armselig.net>
2025-07-07 17:25:14 +0200

    fix(nvim-vscode): get path to config correctly

--- nvim-vscode/init.lua
+++ nvim-vscode/init.lua
@@ -1,6 +1,9 @@
 if vim.g.vscode then
     -- VSCode extension
-    require('nvim-vscode.lazy')
+    -- Add the current directory's lua path to runtime path
+    local current_dir = vim.fn.expand('<sfile>:p:h')
+    vim.opt.rtp:prepend(current_dir)
+    require('config.lazy')
 else
     -- ordinary Neovim
 end
---
+++ nvim-vscode/lua/plugins/init.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "tpope/vim-surround",
+        event = "VeryLazy",
+        config = function()
+            -- No specific configuration needed for vim-surround
+            -- as it works out of the box.
+            -- However, you can customize key mappings if desired.
+            -- Example: vim.api.nvim_set_keymap('n', 'ys', '<Plug>Surround', { noremap = true, silent = true })
+        end,
+        dependencies = {
+            "tpope/vim-repeat", -- Optional: for repeating surround commands
+        },
+        -- Optional: You can add additional settings or key mappings here
+        -- if you want to customize the behavior of vim-surround.
+        -- For example, you can set up custom key mappings for surround commands.
+    },
+    { "tpope/vim-sensible" },
+    { "tpope/vim-fugitive" },
+    { "tpope/vim-commentary" }
+}
