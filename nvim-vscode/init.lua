eaabfbdc90441c3057a02c64ef6e85eb249a9cab
Hendrik <henne@armselig.net>
2025-07-08 10:33:56 +0200

    chore(nvim-vscode): organise plugins

--- nvim-vscode/init.lua
+++ nvim-vscode/init.lua
@@ -3,6 +3,8 @@ if vim.g.vscode then
     -- Add the current directory's lua path to runtime path
     local current_dir = vim.fn.expand('<sfile>:p:h')
     vim.opt.rtp:prepend(current_dir)
+
+    require('config.keys')
     require('config.lazy')
 else
     -- ordinary Neovim
