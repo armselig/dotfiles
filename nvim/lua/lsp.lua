fcb9050c7996850e36d8aa014416b65670f586d3
Hendrik <henne@armselig.net>
2025-07-11 22:48:38 +0200

    refactor(nvim): maybe less lsp conflicts between ts and deno

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -49,6 +49,20 @@ require('mason-lspconfig').setup({
 })
 
 -- lsps
+
+local nvim_lsp = require('lspconfig')
+nvim_lsp.denols.setup {
+    on_attach = on_attach,
+    root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+nvim_lsp.ts_ls.setup {
+    on_attach = on_attach,
+    root_dir = nvim_lsp.util.root_pattern("package.json"),
+    single_file_support = false
+}
+
+
 local lspconfig = require('lspconfig')
 
 lspconfig.lua_ls.setup({
